023e60343dbbba2d2288be3a1e3eae43db199f4a
Ananya02Gupta <ananyagupta315@gmail.com>
2026-08-02 22:12:32 +0530

    Update Nodemailer configuration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,14 +2,15 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ name, email, subject, message }) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
